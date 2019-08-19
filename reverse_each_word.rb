4c695528eddde727785e52059479af6645fc4959
Mike Cooper <mikescooper@icloud.com>
2019-08-19 02:28:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   sentence_array = string.split(" ")
-  new_array = sentence_array.collect{ |word| new_array << "#{word.reverse}"}
+  new_array = sentence_array.collect{ |word| new_array.push("#{word.reverse}")}
   new_array.join(" ")
 end
